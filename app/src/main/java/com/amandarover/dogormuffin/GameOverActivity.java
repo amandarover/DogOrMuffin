7428dfa7051183a9a038246f1bb82b68d615926b
amandarover <rover.amanda@gmail.com>
2017-10-31 21:02:16 -0200

    Listen buttons on Game Over view to be redirected when clicked

--- app/src/main/java/com/amandarover/dogormuffin/GameOverActivity.java
+++ app/src/main/java/com/amandarover/dogormuffin/GameOverActivity.java
@@ -1,13 +1,36 @@
 package com.amandarover.dogormuffin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class GameOverActivity extends AppCompatActivity {
+public class GameOverActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+
+        findViewById(R.id.buttonPlayAgain).setOnClickListener(this);
+        findViewById(R.id.buttonHighScores).setOnClickListener(this);
+        findViewById(R.id.buttonBackToMenu).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonPlayAgain:
+                Intent readyActivity = new Intent(this, ReadyActivity.class);
+                startActivity(readyActivity);
+                break;
+            case R.id.buttonHighScores:
+                Intent highScoreActivity = new Intent(this, HighScoresActivity.class);
+                startActivity(highScoreActivity);
+                break;
+            case R.id.buttonBackToMenu:
+                Intent startActivity = new Intent(this, StartActivity.class);
+                startActivity(startActivity);
+        }
     }
 }
