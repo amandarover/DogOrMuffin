55c1fa33f5dcf72dcb9dcd2d719c59ec761bdf27
amandarover <rover.amanda@gmail.com>
2017-10-31 20:43:41 -0200

    Makes back button on HighScore view to go back to main view

--- app/src/main/java/com/amandarover/dogormuffin/HighScoresActivity.java
+++ app/src/main/java/com/amandarover/dogormuffin/HighScoresActivity.java
@@ -1,13 +1,27 @@
 package com.amandarover.dogormuffin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class HighScoresActivity extends AppCompatActivity {
+public class HighScoresActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_scores);
+
+        findViewById(R.id.buttonBack).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonBack:
+                Intent startActivity = new Intent(this, StartActivity.class);
+                startActivity(startActivity);
+                break;
+        }
     }
 }
