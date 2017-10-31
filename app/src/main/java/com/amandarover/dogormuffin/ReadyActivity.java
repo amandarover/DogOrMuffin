c21a340a8964a14cfba927f1f0a531b3e58dd417
amandarover <rover.amanda@gmail.com>
2017-10-31 20:30:38 -0200

    Click anywhere and be redirected to another page

--- app/src/main/java/com/amandarover/dogormuffin/ReadyActivity.java
+++ app/src/main/java/com/amandarover/dogormuffin/ReadyActivity.java
@@ -1,7 +1,10 @@
 package com.amandarover.dogormuffin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class ReadyActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class ReadyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ready);
+    }
+
+    public void startGame(View view) {
+        Intent intent = new Intent(this, ImageActivity.class);
+        startActivity(intent);
     }
 }
