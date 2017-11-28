6484abddb302c3b7c1f2a70cbe9df07340d20259
amandarover <rover.amanda@gmail.com>
2017-11-28 19:53:02 -0200

    Show final Score when Game Over and input the score in db

--- app/src/main/java/com/amandarover/dogormuffin/GameOverActivity.java
+++ app/src/main/java/com/amandarover/dogormuffin/GameOverActivity.java
@@ -1,9 +1,15 @@
 package com.amandarover.dogormuffin;
 
 import android.content.Intent;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.amandarover.dogormuffin.data.AppDatabase;
+import com.amandarover.dogormuffin.data.Score;
+import com.amandarover.dogormuffin.data.ScoreDao;
 
 public class GameOverActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -11,6 +17,19 @@ public class GameOverActivity extends AppCompatActivity implements View.OnClickL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+
+        Score score = new Score();
+        score.points = getIntent().getIntExtra("points", 0);
+
+        AppDatabase db = AppDatabase.getInstance(this);
+        ScoreDao dao = db.scoreDao();
+        dao.insert(score);
+
+        TextView finalScore = findViewById(R.id.textViewScore);
+        finalScore.setText("Your Score: " + score.points);
+
+        TextView highScore = findViewById(R.id.textViewHighScore);
+        highScore.setText("High Score: " + dao.fetchHighest().points);
 
         findViewById(R.id.buttonPlayAgain).setOnClickListener(this);
         findViewById(R.id.buttonHighScores).setOnClickListener(this);
