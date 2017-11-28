4b1e44fff69be28b4908697878456d955dded761
amandarover <rover.amanda@gmail.com>
2017-11-28 19:41:17 -0200

    Algorithm to change the images randomly
    Verify if choosed image correctly, renderNextImage, getNextRandomImageRes

--- app/src/main/java/com/amandarover/dogormuffin/ImageActivity.java
+++ app/src/main/java/com/amandarover/dogormuffin/ImageActivity.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class ImageActivity extends AppCompatActivity implements View.OnClickListener{
+    ImageView imageGeneric;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
+        imageGeneric = (ImageView) findViewById(R.id.imageGeneric);
+
+        renderNextImage();
 
         findViewById(R.id.buttonDog).setOnClickListener(this);
         findViewById(R.id.buttonMuffin).setOnClickListener(this);
@@ -20,15 +25,46 @@ public class ImageActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.buttonDog:
-                Intent readyActivity = new Intent(this, ReadyActivity.class);
-                startActivity(readyActivity);
+                if (choosed == ImageType.DOG) {
+                    renderNextImage();
+                } else {
+                    Intent gameOverIntent = new Intent(this, GameOverActivity.class);
+                    gameOverIntent.putExtra("points", score);
+                    startActivity(gameOverIntent);
                     finish();
+                }
                 break;
             case R.id.buttonMuffin:
-                Intent highScoreActivity = new Intent(this, HighScoresActivity.class);
-                startActivity(highScoreActivity);
+                if (choosed == ImageType.MUFFIN) {
+                    renderNextImage();
+                } else {
+                    Intent gameOverIntent = new Intent(this, GameOverActivity.class);
+                    gameOverIntent.putExtra("points", score);
+                    startActivity(gameOverIntent);
                     finish();
+                }
                 break;
+        }
+    }
+
+    private void renderNextImage() {
+        imageGeneric.setImageResource(getNextRandomImageRes());
+    }
+
+    int [] dogImages = new int[]{R.drawable.dog1, R.drawable.dog2};
+    int [] muffinImages = new int[]{R.drawable.muffin1, R.drawable.muffin2};
+    enum ImageType {
+        DOG, MUFFIN
+    }
+    ImageType choosed;
+
+    private int getNextRandomImageRes() {
+        if (Math.random()*2 > 1) {
+            choosed = ImageType.DOG;
+            return dogImages[(int)(Math.random()*dogImages.length)];
+        } else {
+            choosed = ImageType.MUFFIN;
+            return muffinImages[(int)(Math.random()*muffinImages.length)];
         }
     }
 }
