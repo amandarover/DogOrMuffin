f8151268e66cfa1cc38a40e379223ca34f17ab11
amandarover <rover.amanda@gmail.com>
2017-12-01 17:49:02 -0200

    Refactor ImageActivity attributes

--- app/src/main/java/com/amandarover/dogormuffin/ImageActivity.java
+++ app/src/main/java/com/amandarover/dogormuffin/ImageActivity.java
@@ -9,17 +9,27 @@ import android.widget.ImageView;
 public class ImageActivity extends AppCompatActivity implements View.OnClickListener{
     int score = 0;
     ImageView imageGeneric;
+    int [] dogImages;
+    int [] muffinImages;
+    enum ImageType {
+        DOG, MUFFIN
+    }
+    ImageType choosed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
-        imageGeneric = (ImageView) findViewById(R.id.imageGeneric);
-
+        initAllComponents();
         renderNextImage();
+    }
 
+    private void initAllComponents() {
+        imageGeneric = findViewById(R.id.imageGeneric);
         findViewById(R.id.buttonDog).setOnClickListener(this);
         findViewById(R.id.buttonMuffin).setOnClickListener(this);
+        dogImages = new int[]{R.drawable.dog1, R.drawable.dog2, R.drawable.dog3, R.drawable.dog4, R.drawable.dog5, R.drawable.dog6, R.drawable.dog7};
+        muffinImages = new int[]{R.drawable.muffin1, R.drawable.muffin2, R.drawable.muffin3, R.drawable.muffin4, R.drawable.muffin5, R.drawable.muffin6, R.drawable.muffin7};
     }
 
     @Override
@@ -53,13 +63,6 @@ public class ImageActivity extends AppCompatActivity implements View.OnClickList
     private void renderNextImage() {
         imageGeneric.setImageResource(getNextRandomImageRes());
     }
-
-    int [] dogImages = new int[]{R.drawable.dog1, R.drawable.dog2, R.drawable.dog3, R.drawable.dog4, R.drawable.dog5, R.drawable.dog6, R.drawable.dog7};
-    int [] muffinImages = new int[]{R.drawable.muffin1, R.drawable.muffin2, R.drawable.muffin3, R.drawable.muffin4, R.drawable.muffin5, R.drawable.muffin6, R.drawable.muffin7};
-    enum ImageType {
-        DOG, MUFFIN
-    }
-    ImageType choosed;
 
     private int getNextRandomImageRes() {
         if (Math.random()*2 > 1) {
